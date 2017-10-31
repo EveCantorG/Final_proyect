d17667373559f03eb3139d2f495aa59d10626e32
EveCantorG <evelyn.cantorg@hotmail.com>
2017-10-31 11:45:02 -0600

    actualizado

---
+++ README.md
@@ -0,0 +1,14 @@
+## FINAL PROYECT ##
+
+1. Solicitar mensaje para cifrar con prompt
+2. Permitir que pueda ingresar mayúsculas o minúsculas
+3. Evitar que pueda ingresar un campo vacío 
+4. Evitar que pueda ingresar numeros
+5. En caso de que agregue alguno de estos datos indicar con un alert que solamente puede ingresar letras
+6. Utilizar en la función la fórmula para cifrar (n- 65 + 33) % 26 + 65
+7. Arrojar mensaje cifrado
+8. Utilizar en la función la fórmula para decifrar(n+ 33)% 26
+
+## FLUJO ##
+
+![Flujo](http://subefotos.com/ver/?8cdd6c2e57c6422c7d23f5e481f0207ao.jpg)
--- js/app.js
+++ js/app.js
@@ -1,15 +1,64 @@
+var message = prompt("¡Coloca tu frase!");/*
+
+/*Solo le permite al usuario letras
+No permite espacios vacíos o números*/
+
+if (/[A-Za-z]/.test(message)===false){
+  alert ('Solamente puedes agregar letras'); 
+}else{
+	(/[A-Za-z]/.test(message)=false)
+}
+
+
+
+/*Función para cifrar el mensaje del prompt
+(x-65+n)%26+65 */
+/*function cipher(message){
+	var messageCipher = "";
+	for(var i=0; i<message.length; i++){
+		var messChar =(message.charCodeAt(i) - 65 + 33) % 26 + 65;
+	}
+	messageCipher = String.fromCharCode(messChar);
+}
+return messageCipher;
+
+/*Función para decifrar el mensaje del prompt*/
+/*function decipher(message){
+	var messageDecipher = "";
+	for(var i=0; i<message.length; i++){
+		var messFromChar =((String.fromCharCode(i) + 33) % 26;
+		messageDecipher = messageDecipher + messFromChar;
+		var finalDecipher = finalDecipher.charCodeAt(messFromChar);
+	}
+	return finalDecipher;
+}
+*/
+
+
+
+
+
+
+
+
+
+
 // Solicitar al usuario el mensaje a codificar
 
-var messageToCipher = prompt("¡Coloca tu frase!");
+/*var messageToCipher = prompt("¡Coloca tu frase!");/*
 
 // Solo le permite al usuario letras
 // No permite espacios vacíos o números
 
-if (/[A-Za-z]/.test(messageToCipher)===false){
-  alert ('Solamente puedes agregar letras');
-  todoCorrecto=false;}
+/*if (/[A-Za-z]/.test(messageToCipher)===false){
+  alert ('Solamente puedes agregar letras'); 
+  todoCorrecto=false;
+}*/
 
-// Cifrar letra por letra
-for(i=0; i < messageToCipher.length; i++){
-  console.log(messageToCipher);
-}
+
+
+
+
+
+
+
